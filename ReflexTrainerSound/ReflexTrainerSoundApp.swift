90abfefdd76e7dfbac10541d6f3dc5dfde1c9e70
Henny2 <henrieke.baunack@web.de>
2024-06-27 20:30:47 -1000

    Initial Commit

---
+++ ReflexTrainerSound/ReflexTrainerSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflexTrainerSoundApp.swift
+//  ReflexTrainerSound
+//
+//  Created by Henrieke Baunack on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ReflexTrainerSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
