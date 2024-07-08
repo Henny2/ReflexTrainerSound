8560a1a8b26ef13d84bc7ed6fbdf72c31ee9019a
Henny2 <henrieke.baunack@web.de>
2024-07-07 14:36:02 -1000

    adding sound categories and an app icon

--- ReflexTrainerSound/AudioPlayerClass.swift
+++ ReflexTrainerSound/AudioPlayerClass.swift
@@ -8,10 +8,27 @@
 import Foundation
 import AVFoundation
 
-class AudioPlayerClass {
+
+// todo: I want to reset the audio session when they stop playing
+
+class AudioPlayerClass: NSObject, AVAudioPlayerDelegate {
     var player: AVAudioPlayer?
     
     func playSound() {
+        // creating an audio session to change how the audio is gonna be played
+        do {
+            // playback:stop existing audio that is playing
+            // ambient: music keeps playing
+            // playback with "duckOthers" is a mix of both
+            //using ambient because I want to be able to listen to music at the same time
+            try AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default)
+            // docking defeats the reflex purpose so not using it here, but together with deactivating the session after playing it would get back to normal volume afterwards
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.duckOthers])
+            try AVAudioSession.sharedInstance().setActive(true)
+        }
+        catch {
+            print("Could not create audio session")
+        }
         guard let path = Bundle.main.path(forResource: "refereeWhistle", ofType:"mp3") else {
             print("Cannot find file")
             return }
@@ -20,10 +37,18 @@ class AudioPlayerClass {
         do {
             self.player = try AVAudioPlayer(contentsOf: url)
             self.player?.play()
+            self.player?.delegate = self 
             print("playing")
             
         } catch let error {
             print(error.localizedDescription)
         }
     }
+    // not gonna enforce the finishing because the time intervals might be too short, so just leaving this here for knowledge :)
+    // this is also the reason I am not resetting the Audio Session as recommended here https://blog.kulman.sk/correctly-playing-audio-in-ios-apps/
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Did finish Playing")
+        // this resets the volume from "dockingOthers" but I don't like it in the reflex setting so not necessary here
+        try! AVAudioSession.sharedInstance().setActive(false)
+       }
 }
--- ReflexTrainerSound/TimerTest.swift
+++ ReflexTrainerSound/TimerTest.swift
@@ -7,8 +7,11 @@
 
 import SwiftUI
 import Combine
+import AVFAudio
 
-// need to make sure the sounds are playing even if silent mode is on
+// idea: adding the icon at the top of the view because it's adorable
+
+// info on playing sounds
 //https://developer.apple.com/documentation/avfaudio/avaudiosession/category/1616509-playback
 
 struct TimerTest: View {
@@ -18,86 +21,86 @@ struct TimerTest: View {
     @State private var lowerLimit = 1.0
     @State private var intervals: [Double] = []
     var audioPlayer = AudioPlayerClass()
-
-     var body: some View {
-         Form {
-             VStack(alignment: .center) {
-                 Text("Interval limits in seconds")
-                 HStack{
-                     Spacer()
-                     TextField("Enter the lower interval limit", value: $lowerLimit, format: .number)
-                         .frame(width: 50)
-                         .multilineTextAlignment(.center)
-                     //                     .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                         .textFieldStyle(.roundedBorder)
-                     
-                     
-                     
-                     
-                     Text("-").foregroundStyle(.secondary)
-                     TextField("Enter the upper interval limit", value: $upperLimit, format: .number)
-                         .frame(width: 50)
-                         .textFieldStyle(.roundedBorder)
-                         .multilineTextAlignment(.center)
-                     //                     .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                     Spacer()
-                 }
-                 Button(action: {
-                     if self.isRunning {
-                         self.stopTimer()
-                     } else {
-                         self.startTimer()
-                     }
-                     self.isRunning.toggle()
-                 }) {
-                     Text(self.isRunning ? "Stop" : "Start")
-                         .padding()
-                 }
-                 .disabled(upperLimit<lowerLimit)
-                 .buttonStyle(BorderlessButtonStyle()) // so that only clicking in the frame triggers the button
-                 Button("Reset", action: reset)
-                 .disabled(intervals.count<=0 || isRunning)
-                 .buttonStyle(BorderlessButtonStyle())
-             }
-             // make another VStack and print the trigger times of the run through
-             Section("Intervals") {
-                 List() {
-                     ForEach(intervals, id: \.self) {
-                             Text("\($0, specifier: "%.1f")")
-                         }
-                     
-                 }
-             }
-         }
+    
+    
+    var body: some View {
+        Form {
+            VStack(alignment: .center) {
+                Text("Interval limits in seconds")
+                HStack{
+                    Spacer()
+                    TextField("Enter the lower interval limit", value: $lowerLimit, format: .number)
+                        .frame(width: 50)
+                        .multilineTextAlignment(.center)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    
+                    
+                    
+                    Text("-").foregroundStyle(.secondary)
+                    TextField("Enter the upper interval limit", value: $upperLimit, format: .number)
+                        .frame(width: 50)
+                        .textFieldStyle(.roundedBorder)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                }
+                Button(action: {
+                    if self.isRunning {
+                        self.stopTimer()
+                    } else {
+                        self.startTimer()
+                    }
+                    self.isRunning.toggle()
+                }) {
+                    Text(self.isRunning ? "Stop" : "Start")
+                        .padding()
+                }
+                .disabled(upperLimit<lowerLimit)
+                .buttonStyle(BorderlessButtonStyle()) // so that only clicking in the frame triggers the button
+                Button("Reset", action: reset)
+                    .disabled(intervals.count<=0 || isRunning)
+                    .buttonStyle(BorderlessButtonStyle())
+            }
+            // make another VStack and print the trigger times of the run through
+            Section("Intervals") {
+                List() {
+                    ForEach(intervals, id: \.self) {
+                        Text("\($0, specifier: "%.1f")")
+                    }
+                    
+                }
+            }
+        }
         
-     }
+    }
     private func reset() {
         intervals = []
         upperLimit = 5.0
         lowerLimit = 1.0
     }
-
-     private func startTimer() {
-         scheduleTimer()
-     }
-
-     private func stopTimer() {
-         timer?.cancel()
-         timer = nil
-     }
-
-     private func scheduleTimer() {
-         let randomInterval = Double.random(in: lowerLimit...upperLimit) // Random interval between 1 and 5 seconds
-         timer = Timer.publish(every: randomInterval, on: .main, in: .common)
-             .autoconnect()
-             .sink { _ in
-                 audioPlayer.playSound()
-                 print("Timer triggered at interval: \(randomInterval) seconds")
-                 intervals.append(randomInterval)
-                 self.timer?.cancel()
-                 self.scheduleTimer() // Schedule the timer again with a new random interval
-             }
-     }
+    
+    private func startTimer() {
+        scheduleTimer()
+    }
+    
+    private func stopTimer() {
+        timer?.cancel()
+        timer = nil
+    }
+    
+    private func scheduleTimer() {
+        let randomInterval = Double.random(in: lowerLimit...upperLimit) // Random interval between 1 and 5 seconds
+        timer = Timer.publish(every: randomInterval, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                audioPlayer.playSound()
+                print("Timer triggered at interval: \(randomInterval) seconds")
+                intervals.append(randomInterval)
+                self.timer?.cancel()
+                self.scheduleTimer() // Schedule the timer again with a new random interval
+            }
+        
+    }
 }
 
 #Preview {
